586623669c035c21652df568442c31aded919daa
miyamoto <miyamotok0105@gmail.com>
2020-09-15 11:12:38 +0900

    [Add] flask

---
+++ app.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+import json
+import os
+from flask import Flask, jsonify, make_response, request, Response
+from flask import Flask, request, jsonify, abort, render_template
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080/')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+@app.route('/status', methods=['GET'])
+def status():
+  return jsonify({'message': 'OK'})
+
+@app.route('/')
+def home():
+    return render_template('index_vue.html', title='home')
+    # return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def post_camera():
+  print(request.files)
+  if 'file' not in request.files:
+        return abort(400, {'message': 'ファイルは必須です'})
+  fileStorageObj = request.files['file']
+  filename = secure_filename(fileStorageObj.filename)
+  fileStorageObj.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  return jsonify({'message': 'アップロード成功'})
+
+
+if __name__ == "__main__":
+  app.run()
--- webpack.config.js
+++ webpack.config.js
@@ -4,57 +4,59 @@ const fs = require('fs-extra');
 const colors = require('colors/safe');
 const formidable = require('formidable');
 
+// webpack-dev-serverでnpm startでも動く。
+// flaskでも動くようにした。
 
-module.exports = {
-    entry: `./server-side.js`,
-    devServer: {
-        // webpack-dev-server middleware
-        before(app) {
+// module.exports = {
+//     entry: `./server-side.js`,
+//     devServer: {
+//         // webpack-dev-server middleware
+//         before(app) {
 
-            // =============================================
-            // file upload handler for simple upload example
-            // =============================================
-            // make sure 'uploads' directory exists (and
-            // create it otherwise)
-            const targetDir = 'uploads';
-            fs.ensureDirSync(targetDir);
+//             // =============================================
+//             // file upload handler for simple upload example
+//             // =============================================
+//             // make sure 'uploads' directory exists (and
+//             // create it otherwise)
+//             const targetDir = 'uploads';
+//             fs.ensureDirSync(targetDir);
 
-            // file upload handler for examples
-            app.post('/upload', (req, res) => {
-                // save uploaded file
-                let form = new formidable.IncomingForm();
-                form.uploadDir = targetDir;
-                form.keepExtensions = true;
+//             // file upload handler for examples
+//             app.post('/upload', (req, res) => {
+//                 // save uploaded file
+//                 let form = new formidable.IncomingForm();
+//                 form.uploadDir = targetDir;
+//                 form.keepExtensions = true;
 
-                console.log('saving uploaded file...');
+//                 console.log('saving uploaded file...');
 
-                form.on('error', (err) => {
-                    console.log(colors.red('upload error:'));
-                    console.log(err);
-                });
+//                 form.on('error', (err) => {
+//                     console.log(colors.red('upload error:'));
+//                     console.log(err);
+//                 });
 
-                form.on('fileBegin', (name, file) => {
-                    // use original filename in this example
-                    file.path = form.uploadDir + '/' + file.name;
-                    console.log(colors.yellow('filename:', file.name));
-                });
+//                 form.on('fileBegin', (name, file) => {
+//                     // use original filename in this example
+//                     file.path = form.uploadDir + '/' + file.name;
+//                     console.log(colors.yellow('filename:', file.name));
+//                 });
 
-                form.on('end', () => {
-                    console.log(colors.green('saved file.'));
-                    console.log('');
-                });
+//                 form.on('end', () => {
+//                     console.log(colors.green('saved file.'));
+//                     console.log('');
+//                 });
 
-                form.parse(req, (err, fields, files) => {
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    res.write('received upload:\n\n');
-                    res.end(util.inspect({fields: fields, files: files}));
-                });
+//                 form.parse(req, (err, fields, files) => {
+//                     res.writeHead(200, {'content-type': 'text/plain'});
+//                     res.write('received upload:\n\n');
+//                     res.end(util.inspect({fields: fields, files: files}));
+//                 });
 
-                return;
-            });
-            console.log('');
-            console.log(colors.green(' [examples] /upload handler ready'));
-            console.log('');
-        }
-    }
-}
+//                 return;
+//             });
+//             console.log('');
+//             console.log(colors.green(' [examples] /upload handler ready'));
+//             console.log('');
+//         }
+//     }
+// }
